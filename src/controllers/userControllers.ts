af9303414d31922572ac051f9218ec7555ec0f11
Jessica Sampaio <jesssampaio@icloud.com>
2023-07-18 18:51:33 -0400

    create post route

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -4,7 +4,8 @@ import { createUserModel } from '../models/userModels'
 export async function createUser(req: Request, res: Response) {
   try {
     const { firstName, lastName, email, password, profileImg, state, city, country, username } = req.body;
-    await createUserModel!({ firstName, lastName, email, password, profileImg, state, city, country, username })
+    await createUserModel!({ firstName, lastName, email, password, profileImg, state, city, country, username });
+    return res.status(200).send();
 
   } catch (err) {
     return res.send(err);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express, { type Request, type Response } from 'express';
-import userRoutes from './routes/userRoutes'
+import express from 'express';
+import userRoutes from './routes/userRoutes';
+import postRoutes from './routes/postRoutes';
 
 const app = express();
-const PORT = process.env.PORT || 3333
+const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
-app.use(userRoutes)
+app.use(userRoutes);
+app.use(postRoutes);
 
-app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
 
